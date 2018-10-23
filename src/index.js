1eeb6c662fcb6bd4cbb2d9a0df0a3e07529f1558
Emad Jumaah <emadjumaah@gmail.com>
2018-10-23 10:39:19 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,34 +1,13 @@
-const url = require("url");
-var crypto = require("crypto");
-var myurl = "http://ayshee.net/200x200/image1.png";
-var secret = "ayshee.net"; //make this your secret!!
-var algorithm = "sha256"; //consider using sha256
-var hash, hmac;
+import { generateUrl, validateUrl } from "./util";
 
-// Method 2 - Using update and digest:
-hmac = crypto.createHmac(algorithm, secret);
-hmac.update(myurl);
-hash = hmac.digest("hex");
-console.log("Hash: ", hash);
-const finalurl = `${myurl}?${hash}`;
+const url = "http://ayshee.net/200x200/image1.png";
+const secret = "ayshee.net";
 
-console.log("finalurl : ", finalurl);
+const createUrl = generateUrl(url, secret);
 
-const validUrl = uri => {
-  const hash = url.parse(uri).query;
-  const path = uri.split("?").shift();
-
-  const hmac = crypto.createHmac(algorithm, secret);
-  hmac.update(path);
-  const newhash = hmac.digest("hex");
-
-  if (hash === newhash) {
-    return true;
-  }
-  return false;
-};
+console.log(createUrl);
 
 const checkurl =
-  "http://ayshee.net/200x200/image1.png?1b819d020123d9d6eac02571f4c83f71625b027febff7687201cded1e2d1c9a3";
+  "http://ayshee.net/200x200/image1.png?bba5e922b1626a0774c7aedd11a7f1bc1fd5845b";
 
-console.log("Valid", validUrl(checkurl));
+console.log("Valid", validateUrl(checkurl, secret));
---
+++ src/util.js
@@ -0,0 +1,21 @@
+import url from "url";
+import crypto from "crypto";
+
+export const hashOriginalUrl = (url, secret, algorithm = "sha1") => {
+  return crypto
+    .createHmac(algorithm, secret)
+    .update(url)
+    .digest("hex");
+};
+
+export const generateUrl = (urlToHash, secret) => {
+  const hash = hashOriginalUrl(urlToHash, secret);
+  return `${urlToHash}?${hash}`;
+};
+
+export const validateUrl = (uri, secret) => {
+  const hash = url.parse(uri).query;
+  const path = uri.split("?").shift();
+  const newhash = hashOriginalUrl(path, secret);
+  return hash === newhash ? true : false;
+};
